5ea9b6068dd6f2f7bde2ad3951aec818d90b1aa6
CROSS9111 <hirotoshikurosu@gmail.com>
2024-02-04 16:34:05 +0900

    3 commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import Image from "next/image";
+'use client'
+import { useState,useEffect } from 'react';
+// import BarcodeReader from "./components/BarcodeReader";
+// import ProductDisplay from './components/ProductDisplay';
+// import PurchaseList from './components/PurchaseList';
+import TitleBar from './components/TitleBar';
+
+// 型定義
+type Producttype = {
+  PRD_ID: number;
+  PRD_CD: string;
+  PRD_NAME: string;
+  PRD_PRICE: number;
+};
 
 export default function Home() {
+  const [product, setProduct] = useState<Producttype | null>(null);
+  
+
   return (
     <>
-    test
+    <TitleBar/>
+    {product?.PRD_CD}
     </>
   );
 }
+
+// const aaa = {
+//   PRD_ID: 1,
+//   PRD_CD: "a",
+//   PRD_NAME: "bbb",
+//   PRD_PRICE: 22,
+// }
+// useEffect(() => {
+//   setProduct(aaa);
+// }, []);
